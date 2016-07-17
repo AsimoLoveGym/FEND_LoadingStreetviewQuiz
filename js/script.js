3ba927fcab6ac809c52320ff1211d36f4274c928
AsimoLoveGym <xiazhuangz@gmail.com>
2016-07-17 17:28:59 -0500

    Finished, really fun

--- js/script.js
+++ js/script.js
@@ -11,10 +11,31 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var street= $('#street').val();
+    var city= $('#city').val();
+    // console.log(street,city);
+    var location = street + ', ' + city;
+    console.log(location);
+    var source = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location="+location+"&heading=151.78&pitch=-0.76&key=AIzaSyAEYF6NkO2u4X0Ofclgo-6G8BQcFjYLx8k";
+    console.log(source);
+
+    var img = $('<img />',{
+      id: 'Streetview',
+      class: 'bgimg',
+      src: source,
+      alt: 'Streetview'
+    });
+    img.appendTo($('body'));
+
     // load streetview
 
     // YOUR CODE GOES HERE!
-    $body.append('<img class="bgimg" src="https://static.pexels.com/photos/6480/person-woman-girl-model.jpg">');
+    // $body.append('<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location=1600 pennsylvania ave, washington dc&heading=100&pitch=28&&scale=2&key=AIzaSyAEYF6NkO2u4X0Ofclgo-6G8BQcFjYLx8k">');
+
+    // var elem = document.createElement('img');
+    //     elem.className = 'bgimg';
+    //     elem.src = "https://static.pexels.com/photos/6480/person-woman-girl-model.jpg";
+    //     $body.append(elem)
 
     return false;
 };
